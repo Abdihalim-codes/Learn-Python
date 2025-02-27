ac4b10e7d30cf5f32710de9506a95de55ed9e103
Abdihalim Sh <cabdooo19@gmail.com>
2025-02-27 09:35:13 +0300

    Update intermediate_exercise_solutions.py

--- intermediate_exercise_solutions.py
+++ intermediate_exercise_solutions.py
@@ -3,8 +3,8 @@ print("Question 1")
 
 # declare the list
 #           0,      1,  2,         3, 4,    5
-my_list = ["apple", 11, "Orange", 22, "Cat", 33]
-my_list.append("Dog")
+my_list = ["Apple", 101, "Orange", 102, "Cat", 103]
+my_list.append("Ball")
 # Tell the user what your list contains
 print("This list contains numbers and strings.")
 # Tell the user to put and take input from the user and assign to variable
@@ -23,7 +23,7 @@ marks = {
     str(input("Enter course name: ")): int(input("Enter course marks: ")),
     str(input("Enter course name: ")): int(input("Enter course marks: "))
 }
-
+# print out what user entered
 print(f"Your dictionary is: {marks}")
 print(f"Your keys are: {marks.keys()}")
 print(f"Your Values are: {marks.values()}")
